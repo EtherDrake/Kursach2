2c98449c32c84937d3fd01fd3247cc8df83d63c5
EtherDrake <vivern07@gmail.com>
2018-04-15 13:47:27 +0300

    OutlayList refactoring

--- app/src/main/java/com/example/admin/budget3/OutlayList.java
+++ app/src/main/java/com/example/admin/budget3/OutlayList.java
@@ -1,5 +1,7 @@
 package com.example.admin.budget3;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,9 +16,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -36,11 +41,16 @@ import Utility.outlayAdapter;
 
 public class OutlayList extends AppCompatActivity {
 
-    ListView listView;
     User user;
     List<balanceAction> list;
+
+    int myYear, myMonth, myDay;
+    Date actionDate, comparingDate;
     int period, type;
 
+    Spinner spinner;
+    Button calendar;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +64,23 @@ public class OutlayList extends AppCompatActivity {
 
         user = Methods.load(this);
         listView = findViewById(R.id.listView4);
+        spinner = findViewById(R.id.spinner7);
+        calendar = findViewById(R.id.button14);
         list = user.data.balanceActions;
+
+        String[] arraySpinner = new String[] {
+                "День", "Місяць", "Рік", "Весь час"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setSelection(3);
+
+        spinner.setVisibility(View.INVISIBLE);
+        calendar.setVisibility(View.INVISIBLE);
 
         type = getIntent().getIntExtra("type", 0);
 
@@ -73,7 +99,6 @@ public class OutlayList extends AppCompatActivity {
                 sums=new double[user.data.categoriesOutlay.size()];
                 switch (pType) {
                     case 1: {
-
                         for (int i = 0; i < user.data.categoriesOutlay.size(); i++) sums[i] = 0;
 
                         for (int j = 0; j < user.data.balanceActions.size(); j++) {
@@ -240,19 +265,28 @@ public class OutlayList extends AppCompatActivity {
                 });
 
             }
-            else if(type==2)
+            else if(type==2 || type==3)
             {
                 indexes.clear();
+                spinner.setVisibility(View.VISIBLE);
+                calendar.setVisibility(View.VISIBLE);
+
                 String category=getIntent().getStringExtra("cat");
                 for (int i = 0; i < user.data.balanceActions.size(); i++)
                 {
                     if (Objects.equals(user.data.balanceActions.get(i).category, category)) {
-                        balanceAction action=user.data.balanceActions.get(i);
-                        //listToShow.add(Methods.formatDate(action.date)+":"+user.data.balanceActions.get(i).info+"("+format.format(Math.abs(user.data.balanceActions.get(i).amount))+"₴)");
-                        listToShow2.add(user.data.balanceActions.get(i));
-                        indexes.add(i);
+                        if((spinner.getSelectedItemPosition()==0 && user.data.balanceActions.get(i).date.compareTo(comparingDate)==0)
+                                || (spinner.getSelectedItemPosition()==1 && user.data.balanceActions.get(i).date.getMonth()==comparingDate.getMonth() && user.data.balanceActions.get(i).date.getYear()==comparingDate.getYear())
+                                || (spinner.getSelectedItemPosition()==2  && user.data.balanceActions.get(i).date.getYear()==comparingDate.getYear())
+                                || (spinner.getSelectedItemPosition()==3 )) {
+                            balanceAction action = user.data.balanceActions.get(i);
+                            //listToShow.add(Methods.formatDate(action.date)+":"+user.data.balanceActions.get(i).info+"("+format.format(Math.abs(user.data.balanceActions.get(i).amount))+"₴)");
+                            listToShow2.add(user.data.balanceActions.get(i));
+                            indexes.add(i);
+                        }
                     }
                 }
+
 
                 listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                     @Override
@@ -264,89 +298,38 @@ public class OutlayList extends AppCompatActivity {
                         final EditText priceInput = textEntryView.findViewById(R.id.editText9);
                         final EditText infoInput = textEntryView.findViewById(R.id.editText15);
                         final Spinner spinner = textEntryView.findViewById(R.id.spinner3);
+                        final TextView dateOutput = textEntryView.findViewById(R.id.textView41);
+                        final Button changeDate = textEntryView.findViewById(R.id.button13);
 
                         priceInput.setText(String.valueOf(Math.abs(list.get(indexes.get(position)).amount)));
                         infoInput.setText(list.get(indexes.get(position)).info);
+                        ArrayAdapter<String> adapter;
 
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(OutlayList.this,
+                        if(type==2) adapter = new ArrayAdapter<>(OutlayList.this,
                                 android.R.layout.simple_spinner_item, user.data.categoriesOutlay);
-
-                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        spinner.setAdapter(adapter);
-                        spinner.setSelection(user.data.categoriesOutlay.indexOf(list.get(indexes.get(position)).category));
-
-
-                        final AlertDialog.Builder alert = new AlertDialog.Builder(OutlayList.this);
-                        alert.setTitle(
-                                "Редагувати").setView(
-                                textEntryView).setPositiveButton("Зберегти",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        if(!Objects.equals(priceInput.getText().toString(), ""))
-                                        {
-                                            double price=-1*Double.valueOf(priceInput.getText().toString());
-                                            String info=infoInput.getText().toString();
-
-                                            balanceAction toChange=list.get(indexes.get(position));
-                                            toChange.amount=price;
-                                            toChange.info=info;
-                                            toChange.category=user.data.categoriesOutlay.get(spinner.getSelectedItemPosition());
-
-                                            list.set(indexes.get(position),toChange);
-                                            Methods.save(user, OutlayList.this);
-                                            refreshListView(1);
-                                        }
-                                    }
-                                }).setNegativeButton("Видалити",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        list.remove((int)indexes.get(position));
-                                        Methods.save(user, OutlayList.this);
-                                        refreshListView(1);
-                                    }
-                                });
-                        alert.show();
-                        return true;
-                    }
-                });
-            }
-
-            else if(type==3)
-            {
-                indexes.clear();
-                String category=getIntent().getStringExtra("cat");
-                for (int i = 0; i < user.data.balanceActions.size(); i++)
-                {
-                    if (Objects.equals(user.data.balanceActions.get(i).category, category)) {
-                        balanceAction action=user.data.balanceActions.get(i);
-                        //listToShow.add(Methods.formatDate(action.date)+":"+user.data.balanceActions.get(i).info+"("+format.format(Math.abs(user.data.balanceActions.get(i).amount))+"₴)");
-                        listToShow2.add(user.data.balanceActions.get(i));
-                        indexes.add(i);
-                        Log.d("asd", String.valueOf(i));
-                    }
-                }
-
-                listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                        LayoutInflater factory = LayoutInflater.from(OutlayList.this);
-
-                        final View textEntryView = factory.inflate(R.layout.changeoutlay, null);
-
-                        final EditText priceInput = textEntryView.findViewById(R.id.editText9);
-                        final EditText infoInput = textEntryView.findViewById(R.id.editText15);
-                        final Spinner spinner = textEntryView.findViewById(R.id.spinner3);
-
-                        priceInput.setText(String.valueOf(list.get(indexes.get(position)).amount));
-                        infoInput.setText(list.get(indexes.get(position)).info);
-
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(OutlayList.this,
+                        else adapter = new ArrayAdapter<>(OutlayList.this,
                                 android.R.layout.simple_spinner_item, user.data.categoriesIncome);
 
+                        dateOutput.setText(Methods.formatDate(list.get(indexes.get(position)).date));
+                        actionDate=list.get(indexes.get(position)).date;
+
                         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         spinner.setAdapter(adapter);
-                        spinner.setSelection(user.data.categoriesIncome.indexOf(list.get(indexes.get(position)).category));
-                        //Log.d("asd", String.valueOf(list.get(indexes.get(position)).info));
+                        if(type==2)spinner.setSelection(user.data.categoriesOutlay.indexOf(list.get(indexes.get(position)).category));
+                        else spinner.setSelection(user.data.categoriesIncome.indexOf(list.get(indexes.get(position)).category));
+
+                        changeDate.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Date date = new Date();
+                                myYear = date.getYear()+1900;
+                                myMonth = date.getMonth();
+                                myDay = date.getDay();
+                                Log.d("Date issue", String.valueOf(date.getYear()));
+                                showDialog(1);
+                                dateOutput.setText(Methods.formatDate(actionDate));
+                            }
+                        });
 
 
                         final AlertDialog.Builder alert = new AlertDialog.Builder(OutlayList.this);
@@ -357,14 +340,22 @@ public class OutlayList extends AppCompatActivity {
                                     public void onClick(DialogInterface dialog, int whichButton) {
                                         if(!Objects.equals(priceInput.getText().toString(), ""))
                                         {
-                                            double price=Double.valueOf(priceInput.getText().toString());
+                                            double price;
+                                            if(type==2) price=-1*Double.valueOf(priceInput.getText().toString());
+                                            else price=Double.valueOf(priceInput.getText().toString());
+
                                             String info=infoInput.getText().toString();
 
                                             balanceAction toChange=list.get(indexes.get(position));
                                             toChange.amount=price;
                                             toChange.info=info;
-                                            toChange.category=user.data.categoriesIncome.get(spinner.getSelectedItemPosition());
-                                            toChange.updatedAt=new Date();
+                                            if(type==2) toChange.category=user.data.categoriesOutlay.get(spinner.getSelectedItemPosition());
+                                            else toChange.category=user.data.categoriesIncome.get(spinner.getSelectedItemPosition());
+                                            toChange.date=actionDate;
+
+                                            Log.d("debugAmount", String.valueOf(toChange.amount));
+                                            Log.d("debugInfo", toChange.info);
+                                            Log.d("debug", String.valueOf(toChange.category));
 
                                             list.set(indexes.get(position),toChange);
                                             Methods.save(user, OutlayList.this);
@@ -375,7 +366,6 @@ public class OutlayList extends AppCompatActivity {
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int whichButton) {
                                         list.remove((int)indexes.get(position));
-                                        //user.balanceActions= (ArrayList<balanceAction>) list;
                                         Methods.save(user, OutlayList.this);
                                         refreshListView(1);
                                     }
@@ -391,6 +381,25 @@ public class OutlayList extends AppCompatActivity {
             else listView.setAdapter(adapter2);
 
     }
+
+    protected Dialog onCreateDialog(int id)
+    {
+        if (id == 1) {
+            DatePickerDialog tpd = new DatePickerDialog(this, myCallBack, myYear, myMonth, myDay);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener()
+    {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+        {
+            actionDate=new Date(year-1900,monthOfYear,dayOfMonth);
+        }
+    };
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
